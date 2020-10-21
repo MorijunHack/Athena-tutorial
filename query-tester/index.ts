cbaf742d3ccc1b33425312a7a3ca589b7e38156a
jen <40844612+MorijunHack@users.noreply.github.com>
2020-10-22 02:01:07 +0900

    first commit

--- query-tester/index.ts
+++ query-tester/index.ts
@@ -2,6 +2,7 @@ import * as athena from '../athena';
 
 (async () => {
     const queryString = 'SELECT * from mytest.test_table;'
+    // const queryString = 'SELECT * from mytest.test_input;'
     // const queryString = 'SELECT * from athena_tutorial_db.test_input;'
     const aa = await athena.query(queryString);
 
